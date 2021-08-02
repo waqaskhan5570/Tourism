648b899a1a06c88442b3d0cc90a0de714a7d4517
waqaskhan5570 <waqaskhan5570@gmail.com>
2021-08-02 12:21:12 +0500

    New Changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
